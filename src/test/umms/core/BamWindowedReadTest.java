de9355add6c38907ad0ae6ed7ba1d000b4152362
DerrA <DerrA@DERRLAPTOP.ad.umassmed.edu>
2014-09-18 10:31:32 -0400

    Added the ability to read and process multiple alignment files via the -alignments input argument flag.

--- src/test/umms/core/BamWindowedReadTest.java
+++ src/test/umms/core/BamWindowedReadTest.java
@@ -50,9 +50,7 @@ public class BamWindowedReadTest {
 			"\n\t-wellBC <n> well barcode length [default=6]"+
 			"\n\t-UMI <m> UMI length [default=10]";
 	
-	private static int wellBcLength;
-	private static int umiLength;
-	private static boolean scsFlag;
+	private static HashMap<String,ArrayList> bamFiles;     // key=experiment ID, File[]= list of input files for the experiment
 	private static File bamFile;
 	private static File outFile;
 	private static String annotationFile;
@@ -61,6 +59,8 @@ public class BamWindowedReadTest {
 	private static int windowLength;
 	private static int windowOverlap;
 	private static int windowExtend;
+	private static String multimap;     // one of "ignore", "normal" or "scale"
+	private static SAMSequenceCountingDict bamDict;
 	
 	public BamWindowedReadTest(String[] args) throws IOException, ParseException {
 		
@@ -70,62 +70,81 @@ public class BamWindowedReadTest {
 		 */
 		/* seems like a useful utility that can probably be stripped down */
 		ArgumentMap argMap = CLUtil.getParameters(args,usage,"dummy");  /* no default task for now */
-		HashMap<String,HashMap<String,LinkedList<Window>>> countsMap;
-		
-		scsFlag = argMap.isPresent("scs");
-		wellBcLength = argMap.isPresent("wellBC")? argMap.getInteger("wellBC") : 6;
-		umiLength = argMap.isPresent("UMI")? argMap.getInteger("UMI") : 10;
-		
-		/* Windowed read count test parameters */
-		windowLength = argMap.isPresent("wLen")? argMap.getInteger("wLen") : 400;
-		windowOverlap = argMap.isPresent("wOlap")? argMap.getInteger("wOlap") : 40;
-		windowExtend = argMap.isPresent("wExt")? argMap.getInteger("wExt") : 400;
-		
-		bamFile = new File(argMap.getInput());
-		outFile = new File(argMap.getOutput());
-		annotationFile = argMap.getMandatory("annotations");
-		if(!annotationFile.endsWith(".bed") && !annotationFile.endsWith(".BED")){
-			logger.error("Please supply an annotation file in the BED format",new RuntimeIOException());
+		if (!validateArguments(argMap)) {
+			logger.error("Please correct input arguments and retry.");
+			throw new IOException();
 		}
-		
+		HashMap<String,HashMap<String,LinkedList<Window>>> countsMap;
+	
 		/* START TIMING */
 		long startTime = System.nanoTime();
-				
-		/* open the input alignments file */
-//		SAMFileReader bamReader = new SAMFileReader(bamFile, true);   // open as an eager reader
-		SAMFileReader bamReader = new SAMFileReader(bamFile);   // open as a non-eager reader
-
-		bamReader.setValidationStringency(ValidationStringency.LENIENT);	
-		SAMRecordIterator bamIterator = bamReader.iterator();
-
-		SAMFileHeader bamHeader = bamReader.getFileHeader();
-		SAMSequenceCountingDict bamDict = new SAMSequenceCountingDict();
-		bamDict.setLogger(logger);
-		bamDict.copySequences(bamHeader.getSequenceDictionary());    // copy the sequence map from the original dictionary into the counting dict
+		int totalReadCount = 0;
+		int totalInvalidReadCount = 0;
+		int bamFileCount = 0;
+		boolean firstFile = true;      // only read the header from the first alignment file
 		
-		/* read through the input file and increment the count for the start location of each read */
-		// NOTE: Baseline test - simply counting up the number of reads starting at each location for a full
-		//                       10M counts file takes ~30sec
-		int readCount = 0;
-		int invalidReadCount = 0;
-		while (bamIterator.hasNext()) {
-			r = bamIterator.next();
-			if (!r.getReadUnmappedFlag()) {
-				bamDict.updateCount(r);
-				readCount++;
-			} else {
-				invalidReadCount++;
+		/* open the input alignments file */
+		for (String exp:bamFiles.keySet()) {
+			
+			for (int i=0; i<bamFiles.get(exp).size(); i++){
+
+				long loopStartTime = System.nanoTime();    // loop timer
+			
+				// open the next bam file in the list:
+				bamFile = (File) bamFiles.get(exp).get(i);
+				SAMFileReader bamReader = new SAMFileReader(bamFile);   // open as a non-eager reader
+				bamReader.setValidationStringency(ValidationStringency.LENIENT);	
+				SAMRecordIterator bamIterator = bamReader.iterator();
+
+				if (firstFile) {
+					// use the header information in the first bam file to create counts storage
+					SAMFileHeader bamHeader = bamReader.getFileHeader();
+					bamDict = new SAMSequenceCountingDict();
+					bamDict.setLogger(logger);
+					bamDict.copySequences(bamHeader.getSequenceDictionary());    // copy the sequence map from the original dictionary into the counting dict
+					firstFile = false;
+				}
+
+				/* read through the input file and increment the count for the start location of each read */
+				// NOTE: Baseline test - simply counting up the number of reads starting at each location for a full
+				//                       10M counts file takes ~30sec
+				int readCount = 0;
+				int invalidReadCount = 0;
+				while (bamIterator.hasNext()) {
+					r = bamIterator.next();
+					if (!r.getReadUnmappedFlag()) {
+						bamDict.updateCount(r);
+						readCount++;
+					} else {
+						invalidReadCount++;
+					}
+				}
+
+				// close the bam file reader
+				bamReader.close();
+				bamFileCount++;
+				
+				// time the loop:
+				long loopEndTime = System.nanoTime();
+				logger.info("Experiment "+exp+" BAM file "+bamFile+" processed in "+(loopEndTime-loopStartTime)/1e9+" sec\n");
+				logger.info("  "+readCount+" reads\n");
+				logger.info("  "+invalidReadCount+" invalid reads");
+
+				// accumulate counts:
+				totalReadCount+=readCount;
+				totalInvalidReadCount+=invalidReadCount;
 			}
 		}
+		
 		long stopTime = System.nanoTime();
-		logger.info("BAM file "+bamFile+" processed in "+(stopTime-startTime)/1e9+" sec\n");
-		logger.info("  "+readCount+" reads\n");
-		logger.info("  "+invalidReadCount+" invalid reads");
+		logger.info(bamFileCount+" BAM files processed in "+(stopTime-startTime)/1e9+" sec\n");
+		logger.info("  "+totalReadCount+" reads\n");
+		logger.info("  "+totalInvalidReadCount+" invalid reads");
 		long startTime2 = System.nanoTime();
-
+	
 		/* open the input alignments file */
 		Map<String,Collection<Gene>> annotations =  BEDFileParser.loadDataByChr(new File(annotationFile));	
-		
+
 		/* Count all reads beginning within the exons of each of the transcripts in the annotationFile */
 		countsMap = bamDict.countWindowedTranscriptReadStarts(annotations, windowLength, windowOverlap, windowExtend);
 
@@ -143,6 +162,8 @@ public class BamWindowedReadTest {
 		stopTime = System.nanoTime();
 		logger.info("Total processing time: "+(stopTime-startTime)/1e9+" sec\n");
 		
+		
+		// Write the output file:
 		FileWriter writer = new FileWriter(outFile);
 
 		/* write the data in bedGraph format */
@@ -165,12 +186,89 @@ public class BamWindowedReadTest {
 		}
 		writer.flush();
 		writer.close();
-		
-		/* close the files */
-		bamReader.close();
 	}
 
 	public static void main(String[] args) throws ParseException, IOException {
 		new BamWindowedReadTest(args);
+	}
+	
+	private static boolean validateArguments(ArgumentMap argMap) throws IOException {
+		/* Validates the input arguments to ensure that all parameters are consistent and
+		 * fills in provided and default values for all required parameters.
+		 * 
+		 * In the case of single/multiple input alignment files, either the -in <inputFile> flag or
+		 * -alignments <inputFileListFile> can be used. Multiple files (-alignments) overrides -in. 
+		 * The bamFiles argument (a String array) will contain either a single file name (-in), or the
+		 * full set of all alignment files (-alignments). NOTE: In the case of multiple alignments files
+		 * ONLY the header of the first file is used to determine the alignment segments.ls
+		 * 
+		 * 
+		 * @param	args	an ArgumentMap containing the input parameters from the command line.
+		 * @return	success	a boolean flag indicating success/failure of the parameter set validity.
+		 */
+		
+		/* Windowed read count test parameters */
+		windowLength = argMap.isPresent("wLen")? argMap.getInteger("wLen") : 400;
+		windowOverlap = argMap.isPresent("wOlap")? argMap.getInteger("wOlap") : 40;
+		windowExtend = argMap.isPresent("wExt")? argMap.getInteger("wExt") : 400;
+		
+		/* Multimapping parameters */
+		if (!argMap.isPresent("multimap")) {
+			multimap = "ignore";
+		} else {
+			multimap = argMap.get("multimap");
+			if (!(multimap.equals("ignore") || multimap.equals("normal") || multimap.equals("scale"))) {
+				logger.error("-multimap flag must be one of ignore, normal, or scale (is set to "+multimap+")");
+				throw new IOException();
+			}
+		}
+		
+		// Allow multiple inputs 
+		if (argMap.isPresent("alignments")){
+			// The input file file contains a listing of all input files, with an experiment identifier for
+			// each file as: <experimentID>\t<input BAM file>
+			// fill the File array with the list of input files
+			String inputFileFile = argMap.get("alignments");
+			bamFiles = loadBamFileList(inputFileFile);
+		}
+		if (argMap.hasInputFile()) {
+			bamFiles = new HashMap<String, ArrayList>();
+			// Set the default experiment name to "Exp1"
+			bamFiles.put("Exp1",new ArrayList());
+			bamFiles.get("Exp1").add(new File(argMap.getInput()));
+		}
+		outFile = new File(argMap.getOutput());
+		annotationFile = argMap.getMandatory("annotations");
+		if(!annotationFile.endsWith(".bed") && !annotationFile.endsWith(".BED")){
+			logger.error("Please supply an annotation file in the BED format",new RuntimeIOException());
+			return false;
+		}
+			
+		//  
+		return true;   // default return value if all tests pass
+	}
+	
+	private static HashMap<String, ArrayList> loadBamFileList(String fileListFile) 
+		throws IOException {
+		HashMap<String, ArrayList> expBamFiles = new HashMap<String, ArrayList>();
+		
+		BufferedReader br = new BufferedReader(new FileReader(fileListFile));
+		String s;
+		while((s = br.readLine())!= null){
+			String[] strSplit = s.split("\t");
+			//Check for blank lines or comments (start with "#"):
+			if (strSplit.length < 2 || strSplit[0].startsWith("#")) {
+				continue;
+			} 
+			//Map of sample name to alignment File
+			String exp = strSplit[0];
+			File expFile = new File(strSplit[1]);
+			if (!expBamFiles.containsKey(exp)) {
+				expBamFiles.put(exp, new ArrayList());
+			}
+			expBamFiles.get(exp).add(expFile);
+		}		
+		br.close();
+		return expBamFiles;
 	}
 }
