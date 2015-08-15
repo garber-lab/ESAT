02773be4154b5a1d22cd299377cf5400b3be8d76
Alastair Firth <alfirth@gmail.com>
2015-08-15 09:10:48 -0400

    Set up travis CI

---
+++ test/uselessTest/TestTest.java
@@ -0,0 +1,14 @@
+package uselessTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestTest {
+	final boolean truth = true;
+	@Test
+	public void testTrueIsTrue(){
+		assertTrue(truth);
+	}
+}
